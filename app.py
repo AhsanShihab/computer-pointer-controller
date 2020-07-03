3f5d69bc3fbf703d730e07deb16b946ba15230aa
Nazmul Ahsan <ahsan.shihab2@gmail.com>
2020-07-04 00:10:36 +0600

    process output from facial_landmark model

--- app.py
+++ app.py
@@ -53,32 +53,33 @@ def main():
     args = build_argparser().parse_args()
     
     # Load models
-    print(("Loading Face Detection Model."))
+    print("Loading Face Detection Model.")
     face = FaceDetection(model_name=args.face, device=args.device, extensions=args.extension)
     face.load_model()
     print("Face detection model loaded successfully.\n")
 
-    print(("Loading Head Pose Estimation Model."))
+    print("Loading Head Pose Estimation Model.")
     head_pose = HeadPose(model_name=args.head, device=args.device, extensions=args.extension)
     head_pose.load_model()
     print("Head Pose Estimation model loaded successfully.\n")
 
-    print(("Facial Landmarks Detection Model."))
+    print("Facial Landmarks Detection Model.")
     facial_landmark = FacialLandmarks(model_name=args.landmark, device=args.device, extensions=args.extension)
     facial_landmark.load_model()
     print("Facial Landmarks Detection model loaded successfully.\n")
 
-    print(("Gaze Estimation Model."))
+    print("Gaze Estimation Model.")
     gaze = Gaze(model_name=args.gaze, device=args.device, extensions=args.extension)
     gaze.load_model()
     print("Gaze Estimation model loaded successfully.\n")
 
-    print(("All model loaded successfully"))
+    print("All model loaded successfully.\n")
 
     # Create Mouse controller object
     mouse = MouseController(precision="high", speed="fast")
 
     # Read video feed
+    print("Reading video feed.")
     feed = InputFeeder(input_type=args.source, input_file=args.file)
     feed.load_data()
 
@@ -93,26 +94,63 @@ def main():
             # get head pose
             head_position = head_pose.predict(frame)
 
+            # get left and right eye locations
+            left_eye, right_eye = facial_landmark.predict(cropped_face)
+            
+            # crop the eyes
+            left_eye_img = crop_image(left_eye, cropped_face)
+            right_eye_img = crop_image(right_eye, cropped_face)
+
         else:
             pass
 
         if args.results == 1:
+            # draw bounding box around face
             frame = cv2.rectangle(frame, (face_location[0], face_location[1]), (face_location[2], face_location[3]), (0,255,0), 2) 
+            # resize frame
             frame = cv2.resize(frame, (800, 400))
+            # show frame
             cv2.imshow("Frame", frame)
 
         elif args.results == 2:
+            # resize frame
             frame = cv2.resize(frame, (800, 400))
+            # extract info from result
             yaw, pitch, roll = head_position[0, 0], head_position[0, 1], head_position[0, 2]
+            # write results on frame
             cv2.putText(frame, "yaw: " + str(yaw), (5,10), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0), 1)
             cv2.putText(frame, "pitch: " + str(pitch), (5,30), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0), 1)
             cv2.putText(frame, "roll: " + str(roll), (5,50), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0), 1)
-            
+            # show frame
             cv2.imshow("Head Position", frame)
+        
+        elif args.results == 3:
+            # extract info from result
+            # convert eye coordinates from cropped image origin to main frame origin 
+            cropped_img_origin_x = face_location[0]
+            cropped_img_origin_y = face_location[1]
+
+            left_eye_box_xmin = cropped_img_origin_x + left_eye[0]
+            left_eye_box_ymin = cropped_img_origin_y + left_eye[1]
+            left_eye_box_xmax = cropped_img_origin_x + left_eye[2]
+            left_eye_box_ymax = cropped_img_origin_y + left_eye[3]
+
+            right_eye_box_xmin = cropped_img_origin_x + right_eye[0]
+            right_eye_box_ymin = cropped_img_origin_y + right_eye[1]
+            right_eye_box_xmax = cropped_img_origin_x + right_eye[2]
+            right_eye_box_ymax = cropped_img_origin_y + right_eye[3]
+            # draw bounding box around left eye
+            frame = cv2.rectangle(frame, (left_eye_box_xmin, left_eye_box_ymin), (left_eye_box_xmax, left_eye_box_ymax), (0,255,0), 2) 
+            # draw bounding box around right eye
+            frame = cv2.rectangle(frame, (right_eye_box_xmin, right_eye_box_ymin), (right_eye_box_xmax, right_eye_box_ymax), (0,255,0), 2) 
+            # resize frame
+            frame = cv2.resize(frame, (800, 400))
+            # show frame
+            cv2.imshow("Frame", frame)
         
 
         # listening for key press to break, press q to break
-        if cv2.waitKey(25) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             logging.info("Break key pressed") 
             break
     
